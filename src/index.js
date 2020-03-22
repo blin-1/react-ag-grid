95aa06532d1219bcd245fd04f1a2f7f3b7a18c9e
blin-1 <y.monsoon@gmail.com>
2020-03-22 18:19:05 -0400

    Initial

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import GroupGrid from './components/GroupGrid.js'
+ReactDOM.render(<GroupGrid></GroupGrid>, document.getElementById('root'));
